f5a38019526dcc63aeb065ca931b0cbec1a27401
Rimu <rimu.murakami.12@gmail.com>
2023-02-17 23:34:46 +0900

    heap

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,7 @@
 fn main() {
-    // let number = 3;
+    let mut s = String::from("hello");
 
-    // if number < 5 {
-    //     println!("condition was true");
-    // } else {
-    //     println!("condition was false");
-    // }
+    s.push_str(", world!");
 
-    // let condition = true;
-    // let number = if condition { 5 } else { 6 };
-    // println!("The value of number is: {}", number);
-
-    // loop {
-    //     println!("again!");
-    // }
-
-    // let mut count = 0;
-    // 'counting_up: loop {
-    //     println!("count = {}", count);
-    //     let mut remaining = 10;
-
-    //     loop {
-    //         println!("remaining = {}", remaining);
-    //         if remaining == 9 {
-    //             break;
-    //         }
-    //         if count == 2 {
-    //             break 'counting_up;
-    //         }
-    //         remaining -= 1;
-    //     }
-
-    //     count += 1;
-    // }
-    // println!("End count = {}", count);
-
-    for number in (1..4).rev() {
-        println!("{}", number);
-    }
-    println!("LIFTOFF!!");
+    println!("{}", s);
 }

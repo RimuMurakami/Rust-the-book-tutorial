b877a227986e4d5c2d507b2c5a3e68c90f69297a
Rimu <rimu.murakami.12@gmail.com>
2023-02-17 21:55:43 +0900

    for

--- src/main.rs
+++ src/main.rs
@@ -15,23 +15,28 @@ fn main() {
     //     println!("again!");
     // }
 
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {}", count);
-        let mut remaining = 10;
+    // let mut count = 0;
+    // 'counting_up: loop {
+    //     println!("count = {}", count);
+    //     let mut remaining = 10;
 
-        loop {
-            println!("remaining = {}", remaining);
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
+    //     loop {
+    //         println!("remaining = {}", remaining);
+    //         if remaining == 9 {
+    //             break;
+    //         }
+    //         if count == 2 {
+    //             break 'counting_up;
+    //         }
+    //         remaining -= 1;
+    //     }
 
-        count += 1;
+    //     count += 1;
+    // }
+    // println!("End count = {}", count);
+
+    for number in (1..4).rev() {
+        println!("{}", number);
     }
-    println!("End count = {}", count);
+    println!("LIFTOFF!!");
 }

c646916aacac1b03bb99b2d001e75a566ea54e51
Rimu <rimu.murakami.12@gmail.com>
2023-02-17 21:27:42 +0900

    loops within loops

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,31 @@ fn main() {
     //     println!("condition was false");
     // }
 
-    let condition = true;
-    let number = if condition { 5 } else { 6 };
-    println!("The value of number is: {}", number);
+    // let condition = true;
+    // let number = if condition { 5 } else { 6 };
+    // println!("The value of number is: {}", number);
+
+    // loop {
+    //     println!("again!");
+    // }
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
 }

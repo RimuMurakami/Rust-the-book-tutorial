8aa45c06e599a38a47d6daa71427878dde3c733c
Rimu <rimu.murakami.12@gmail.com>
2023-02-18 01:01:21 +0900

    dangle

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
-fn main() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
+// fn main() {
+//     let mut s = String::from("hello");
+//     change(&mut s);
+// }
 
-    println!("The length of '{}' is {}.", s1, len);
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
+// fn main() {
+//     let mut s = String::from("hello");
+
+//     let r1 = &s;
+//     let r2 = &s;
+
+//     println!("{}, {}", r1, r2);
+// }
+
+fn main() {
+    let reference_to_nothing = dangle();
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn dangle() -> String {
+    let s = String::from("hello");
+
+    s
 }

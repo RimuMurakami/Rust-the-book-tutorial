099d80c61728bd68af13c1f184a59fb196c41602
Rimu <rimu.murakami.12@gmail.com>
2023-02-17 19:30:03 +0900

    array

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
+use std::io;
 fn main() {
-    let tup = (500, 6.4, 1);
+    // let x = (500, 6.4, 1);
 
-    let (x, y, z) = tup;
+    // let five_hundred = x.0;
 
-    println!("The value of y is: {}", y);
+    // println!("{}", five_hundred);
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered wad not a number.");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }

1e66028a1a074fa71b24bee1657f80658c4744e4
Rimu <rimu.murakami.12@gmail.com>
2023-02-18 23:12:20 +0900

    6.1 Defining an Enum

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,44 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn _area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            height: size,
-        }
-    }
-}
-
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 69,
-        height: 45,
-    };
+    let some_number = Some(5);
+    let some_string = Some("a String");
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    let absent_number: Option<i32> = None;
 
-    println!("rect1 is {:#?}", &rect2);
+    println!("{:?}, {:?}, {:?}", some_number, some_string, absent_number);
 
-    let sq = Rectangle::square(3);
-    println!("sq is {:#?}", &sq);
+    // #[derive(Debug)]
+    // enum Message {
+    //     Quit,
+    //     Move { x: i32, y: i32 },
+    //     Write(String),
+    //     ChangeColor(i32, i32, i32),
+    // }
 
-    // println!(
-    //     "The area of the rectangle is {} square pixels.",
-    //     rect1.area()
-    // );
+    // impl Message {
+    //     fn call(&self) {
+    //         // method body would be defined here
+    //         // メソッド本体はここに定義される
+    //     }
+    // }
+
+    // let m = Message::Write(String::from("hello"));
+
+    // println!("{:#?}", m::Write);
+
+    // #[derive(Debug)]
+    // enum IpAddr {
+    //     V4(u8, u8, u8, u8),
+    //     V6(String),
+    // }
+
+    // let home = IpAddr::V4(127, 0, 0, 1);
+    // println!("{:#?}", home);
+
+    // enum IpAddrKind {
+    //     V4,
+    //     V6,
+    // }
+
+    // let four = IpAddrKind::V4;
+    // let six = IpAddrKind::V6;
 }
-
-// fn area(rectangle: &Rectangle) -> u32 {
-//     rectangle.width * rectangle.height
-// }

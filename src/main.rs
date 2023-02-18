dbd6d1b960ed631abb65a0ade7e32bbe4e266499
Rimu <rimu.murakami.12@gmail.com>
2023-02-18 18:11:53 +0900

    5.2 Structs-exam

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,50 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
 
-struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// fn main() {
+//     let rect1 = (30, 50);
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(rect1)
+//     );
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
 fn main() {
-    let mut user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("some_username123"),
-        active: true,
-        sign_in_count: 1,
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
     };
 
-    user1.email = String::from("another_email@example.com");
+    println!("rect1 is {:#?}", rect1);
 
-    let user2 = User {
-        email: String::from("another@example.com"),
-        username: String::from("another_username567"),
-        ..user1
-    };
-
-    println!("{}", user2.username);
-
-    let black = Color(0, 0, 0);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
 }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
